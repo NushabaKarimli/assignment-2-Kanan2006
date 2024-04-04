c3dabcc3d8c6e54236753dca98cb27d4c665644b
Kanan4109 <165500813+Kanan4109@users.noreply.github.com>
2024-04-05 00:02:43 +0400

    Update Task_1.py

--- Task_1.py
+++ Task_1.py
@@ -5,9 +5,9 @@ def main():
   if a=="a" or a=="e" or a=="i" or a=="o" or a=="u":
       print ("Entered alphabet is a vowel!")
   elif a=="y":
-      print ("Sometimes it is a vowel, and sometimes it is a consonant!")
+      print("Sometimes it is a vowel, and sometimes it is a consonant!")
   else:
-      print ("Entered alphabet is a consonant!")
+      print("Entered alphabet is a consonant!")
   pass
 
 if __name__ == "__main__":
