4ec726eb62f27eef161899af817de57ab8da3db3
Kanan4109 <165500813+Kanan4109@users.noreply.github.com>
2024-04-05 00:08:10 +0400

    Update Task_2.py

--- Task_2.py
+++ Task_2.py
@@ -1,10 +1,43 @@
 #import libraries here
 
 def main():
-  '''
-  Kodunuzu buraya yazin.
-  '''
-  pass
+    m=str(input("Enter name of the month [ex. June]: "))
+    d=int(input("Enter the day [ex. 5]: "))
+    if m=="January" or m=="February" or m=="March":
+        if m=="March":
+            if d<20:
+                print(f"{m} {d} is in Winter")
+            else:
+                print(f"{m} {d} is in Spring")
+        else:
+            print(f"{m} {d} is in Winter")
+    elif m=="April" or m=="May" or m=="June":
+        if m=="June":
+            if d<21:
+                print(f"{m} {d} is in Spring")
+            else:
+                print(f"{m} {d} is in Summer")
+        else:
+            print(f"{m} {d} is in Spring")
+    elif m=="July" or m=="August" or m=="September":
+        if m=="September":
+            if d<22:
+                print(f"{m} {d} is in Summer")
+            else:
+                print(f"{m} {d} is in Fall")
+        else:
+            print(f"{m} {d} is in Summer")
+    elif m=="October" or m=="November" or m=="December":
+        if m=="December":
+            if d<21:
+                print(f"{m} {d} is in Fall")
+            else:
+                print(f"{m} {d} is in Winter")
+        else:
+            print(f"{m} {d} is in Fall")
+    else:
+        print(f"The data entered is wrong.")
+    pass
 
 if __name__ == "__main__":
   main()
